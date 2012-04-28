31e491ce0410a91260fad416eecab5950b8e655b
Nils Wloka <mail@nilswloka.com>
2012-04-29 01:10:33 +0200

    Changed configuration to work with travis

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.0.4"]
                  [leiningen-core "2.0.0-preview3"]]
-  :profiles {:test {:dependencies [[commons-io "2.0"]]}}
+  :profiles {:test {:dependencies [[commons-io "2.0"]
+                                   [lein-cucumber "1.0.1-SNAPSHOT"]]}}
   :plugins [[lein-cucumber "1.0.0"]]
   :eval-in :leiningen
   :license {:name "Unlicense"
