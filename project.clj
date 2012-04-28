30a94cb2a822d3977c3fb5bf6542e54eb8ae2be7
Nils Wloka <mail@nilswloka.com>
2012-04-29 00:19:16 +0200

    Added lein-cucumber to plugin section to build with travis-ci

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[info.cukes/cucumber-clojure "1.0.4"]
                  [leiningen-core "2.0.0-preview3"]]
   :profiles {:test {:dependencies [[commons-io "2.0"]]}}
+  :plugins [[lein-cucumber "1.0.0"]]
   :eval-in :leiningen
   :license {:name "Unlicense"
             :url "http://unlicense.org/"
