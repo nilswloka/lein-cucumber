d7a5074054d9b4ab5ffb50200a8320a125c58afc
Nils Wloka <mail@nilswloka.com>
2012-04-29 00:41:13 +0200

    New snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cucumber "1.0.0"
+(defproject lein-cucumber "1.0.1-SNAPSHOT"
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.0.4"]
                  [leiningen-core "2.0.0-preview3"]]
