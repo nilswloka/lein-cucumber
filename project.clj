7c73aead6f94ae1eeac7c5ee94557cb969ecf86a
shaolang <shaolang@ymail.com>
2012-12-25 00:25:42 +0800

    Bumped up Clojure to 1.5.0-RC1.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.0.14"]
                  [leiningen-core "2.0.0-preview10"]
-                 [org.clojure/clojure "1.5.0-alpha5"]]
+                 [org.clojure/clojure "1.5.0-RC1"]]
   :profiles {:test {:dependencies [[commons-io "2.0"]]
                     :plugins [[lein-cucumber "1.0.2-SNAPSHOT"]]}}
   :eval-in :leiningen
