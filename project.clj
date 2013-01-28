d1b44f7b56bb4691f8c5ad4519bad380af11853b
Nils Wloka <mail@nilswloka.com>
2013-01-28 22:47:07 +0100

    Updated clojure and leiningen-core dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lein-cucumber "1.0.2"
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.1.1"]
-                 [leiningen-core "2.0.0-preview10"]
-                 [org.clojure/clojure "1.5.0-RC1"]]
+                 [leiningen-core "2.0.0"]
+                 [org.clojure/clojure "1.5.0-RC3"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
                         :plugins [[lein-cucumber "1.0.2"]]}}
   :eval-in :leiningen
