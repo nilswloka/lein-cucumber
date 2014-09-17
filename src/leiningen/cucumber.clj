d4b6dac57a4c5c4bd09cb9e973dde7190628bba7
Giribabu Penki <GIRIBA@yousee.dk>
2014-09-17 10:36:40 +0200

    Update cucumber-clojure version

--- src/leiningen/cucumber.clj
+++ src/leiningen/cucumber.clj
@@ -34,7 +34,7 @@
        (-> project
            (update-in [:dependencies] conj
                       ['lein-cucumber "1.0.2"]
-                      ['info.cukes/cucumber-clojure "1.1.1"])
+                      ['info.cukes/cucumber-clojure "1.1.8"])
            (update-in [:source-paths] (partial apply conj) glue-paths))
        `(do
           (let [~runtime (leiningen.cucumber.util/run-cucumber! ~feature-paths ~glue-paths ~target-path ~(vec args))]
