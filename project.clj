ed672f78ec89137ad89da61b8d37c5095f51dfc6
Giribabu Penki <GIRIBA@yousee.dk>
2014-09-17 10:27:38 +0200

    Update cucumber-clojure version
    Several features like ability to use Before for specific tagged scenarios

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-cucumber "1.0.2"
   :description "Run cucumber-jvm specifications with leiningen"
-  :dependencies [[info.cukes/cucumber-clojure "1.1.1"]
+  :dependencies [[info.cukes/cucumber-clojure "1.1.8"]
                  [leiningen-core "2.0.0"]
                  [org.clojure/clojure "1.5.0-RC3"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
