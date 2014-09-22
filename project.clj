567761475d03fd8c282a2ae52dabd1ce0aaab35d
GIRIBA <giriba@yousee.dk>
2014-09-22 15:46:13 +0200

    eval-in removed so compile :all works

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojure "1.5.0-RC3"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
                         :plugins [[lein-cucumber "1.0.3-giri"]]}}
-  :eval-in :leiningen
+  
   :license {:name "Unlicense"
             :url "http://unlicense.org/"
             :distribution :repo}
