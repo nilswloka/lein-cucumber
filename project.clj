2259ffd2669f4ee1c231b38010a9ded7b177f614
GIRIBA <giriba@yousee.dk>
2014-09-24 10:49:48 +0200

    bumper vesrion

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-cucumber "1.0.3-gip"
+(defproject lein-cucumber "1.0.4-giri"
   :description "Run cucumber-jvm specifications with leiningen"
   :dependencies [[info.cukes/cucumber-clojure "1.1.8"]
                  [leiningen-core "2.0.0"]
                  [org.clojure/clojure "1.5.0-RC3"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
-                        :plugins [[lein-cucumber "1.0.3-gip"]]}}
+                        :plugins [[lein-cucumber "1.0.4-giri"]]}}
   
   :license {:name "Unlicense"
             :url "http://unlicense.org/"
